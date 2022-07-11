569886b73296a825e998cab6a4b2b14517d16eec
RipeCherries <alexeyegoshin0403@mail.ru>
2022-07-11 19:44:10 +0700

    Made function for print information about project

---
+++ main.py
@@ -0,0 +1,17 @@
+# create by Egoshin Alexey 07/2022
+# encoding: UTF-8
+
+
+def show_info():
+    print(
+        "Шифр Цезаря.",
+        "Автор: Алексей Егошин alexegoshin0403@yandex.ru",
+        sep='\n')
+
+
+def main():
+    show_info()
+
+
+if __name__ == '__main__':
+    main()
