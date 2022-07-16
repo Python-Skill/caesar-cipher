0960d3550ec5677b5dd35a640dc9d7314242f25f
RipeCherries <alexeyegoshin0403@mail.ru>
2022-07-16 18:08:38 +0700

    Made the encryption and decryption function

--- main.py
+++ main.py
@@ -9,6 +9,29 @@ def show_info():
         sep='\n')
 
 
+def caesar_cipher(value: str, key: int):
+    alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+
+    result = ""
+
+    for i in range(len(value)):
+        if value[i] in alphabet:
+            index = alphabet.find(value[i])
+
+            index += key
+
+            if index > len(alphabet):
+                index -= len(alphabet)
+            elif index < 0:
+                index += len(alphabet)
+
+            result += alphabet[index]
+        else:
+            result += value[i]
+
+    return result
+
+
 def main():
     show_info()
 
